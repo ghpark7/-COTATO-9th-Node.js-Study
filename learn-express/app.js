9b952a6640f7a191d7dbfea2ee5d06ff1b31f0fe
Geonhwi Park <ghpark715@gmail.com>
2024-03-29 16:59:56 +0900

    nunjucks 사용
    nunjucks 설치 후 사용

--- learn-express/app.js
+++ learn-express/app.js
@@ -4,12 +4,20 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
+const nunjucks = require('nunjucks');
 
 dotenv.config();
-const app = express();
 const indexRouter = require('./routes');
 const userRouter = require('./routes/user');
+
+const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -31,7 +39,9 @@ app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('Not Found');
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
 });
 
 app.use((req, res, next) => {
@@ -47,8 +57,10 @@ app.get('/', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message);
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(app.get('port'), () => {
