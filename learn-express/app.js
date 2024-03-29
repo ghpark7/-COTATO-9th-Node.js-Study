14d1f5c3ba0d74129939984663109b71b03e9473
Geonhwi Park <ghpark715@gmail.com>
2024-03-29 16:41:03 +0900

    router 학습
    router 학습

--- learn-express/app.js
+++ learn-express/app.js
@@ -7,6 +7,8 @@ const path = require('path');
 
 dotenv.config();
 const app = express();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
@@ -24,6 +26,13 @@ app.use(session({
     },
     name: 'session-cookie',
 }));
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
 
 app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됩니다.');
